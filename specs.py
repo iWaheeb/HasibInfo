eb78d223febf0213746cec072a4459296e74c01f
waheeb404 <waheebbasalem@gmail.com>
2024-02-01 08:41:01 +0300

    added OS, storage, and ram information

--- specs.py
+++ specs.py
@@ -1,55 +1,34 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5 import uic
+import sys
+
+import platform
+import psutil
+import GPUtil
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        # Load the UI
+        uic.loadUi("specs.ui", self)
+
+        # Define our widgets
+        self.cpu_label = self.findChild(QLabel, "label")
+        self.storage_label = self.findChild(QLabel, "label_2")
+        self.ram_label = self.findChild(QLabel, "label_3")
+
+        # Do something
+        self.cpu_label.setText(f"Operating System: {platform.system()} {platform.release()}")
+        self.storage_label.setText(f"Storage: {round(psutil.disk_usage('/').total / (1024**3), 2)} GB")
+        self.ram_label.setText(f"RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB")
+        GPUtil.getGPUs()
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(294, 327)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(30, 40, 241, 25))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(30, 200, 231, 25))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(34, 120, 241, 25))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_3")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 294, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        # show the app
+        self.show()
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "HasibInfo"))
-        self.label.setText(_translate("MainWindow", "المعالج"))
-        self.label_2.setText(_translate("MainWindow", "الرامات"))
-        self.label_3.setText(_translate("MainWindow", "كرت الشاشة"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+# Initialize the app
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
